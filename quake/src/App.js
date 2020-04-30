f931686e2babe7e319706e74be1ad5961b1bae52
garybot <argot.pryer@gmail.com>
2020-04-29 21:49:06 -0700

    Added map to route.

--- quake/src/App.js
+++ quake/src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
-
 import Register from './components/Register';
 import Login from './components/Login';
 import WorldMap from './components/WorldMap';
@@ -12,14 +11,11 @@ function App() {
   return (
     <div className="App">
       <h1>Quake!</h1>
-     <Router>
-
-
-       <Route exact path = '/register' component ={Register}/>
-       <Route exact path = '/login' component ={Login}/>
-       <WorldMap />
-
-     </Router>
+      <Router>
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/' component={WorldMap} />
+      </Router>
     </div>
   );
 }

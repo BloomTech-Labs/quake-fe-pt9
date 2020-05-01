d93e011c82a3bf8e52ccc88cb832fa0559403f24
RyanSWest <west56309@gmail.com>
2020-04-30 22:38:28 -0500

    more styling

--- quake/src/components/Nav.js
+++ quake/src/components/Nav.js
@@ -8,6 +8,8 @@ const Nav = ()=> {
     return(
 
         <div className = 'nav'>
+                    <NavLink className = 'navie'to = "/">Home</NavLink>
+
         <NavLink className = 'navie'to = "/Login">Login</NavLink>
         <NavLink className = 'navie' to = "/register">Register</NavLink>
         <NavLink className = 'navie'to = "/contact">Contact</NavLink>

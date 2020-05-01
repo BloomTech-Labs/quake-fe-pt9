51b43938f2f3bd3be4eb85f8aaf2623b37a633ed
garybot <argot.pryer@gmail.com>
2020-04-30 19:16:51 -0700

    refactored leaflet map component to be more reactful

--- quake/src/components/WorldMap.js
+++ quake/src/components/WorldMap.js
@@ -2,38 +2,39 @@ import React, {Component} from 'react';
 import L from 'leaflet';
 import {Map, TileLayer, GeoJSON} from 'react-leaflet';
 
-import data from '../data/2.5_month.geojson.json'
-
-function filterByMag(data, mag) {
-  return data.features.filter((feature) => {
-    return feature.properties.mag >= mag;
-  })
-}
-
-const geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff0000",
-    color: "#000",
-    weight: 1,
-    opacity: .5,
-    fillOpacity: 0.5
-};
-
-const quakeStyles = (feature, latlng) => {
-  return L.circleMarker(latlng, geojsonMarkerOptions);
-}
-
+import data from '../data/2.5_month.geojson.json';
 
 class WorldMap extends Component {
   constructor() {
-     super()
-     this.state = {
-       lat: 0,
-       lng: 0,
-       zoom: 1,
-       quakes: filterByMag(data, 4.5)
-     }
-   }
+    super();
+    this.state = {
+      lat: 0,
+      lng: 0,
+      zoom: 1,
+      quakes: data, // geoJSON data
+      mag: 5, // minimun magnitude to display on map.
+      geojsonMarkerOptions: { // These are options to be passed to markerStyles().
+        radius: 8,
+        fillColor: "#f00", // red
+        color: "#000",
+        weight: 1,
+        opacity: 0.5,
+        fillOpacity: 0.5
+      },
+    };
+    // fn passed to the filter prop of react-leaflets GeoJSON component.
+    this.filterByMag = (feature, layer) => {
+      return feature.properties.mag >= this.state.mag;
+    };
+    // styles fn to pass to pointToLayer() to have the quakes appear as red circles.
+    // the .bindPopup() creates a popup for each circle showing the quake titles.
+    this.markerStyles =  (feature, latlng) => {
+      return L.circleMarker(latlng, this.state.geojsonMarkerOptions)
+        .bindPopup(function (layer) {
+          return feature.properties.title;
+        });
+    };
+  }
 
   render() {
     const position = [this.state.lat, this.state.lng];
@@ -43,7 +44,11 @@ class WorldMap extends Component {
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <GeoJSON data={this.state.quakes} pointToLayer={quakeStyles}/>
+        <GeoJSON
+          data={this.state.quakes}
+          pointToLayer={this.markerStyles}
+          filter={this.filterByMag}
+        />
       </Map>
     )
   }

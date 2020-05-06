6599f939876193f9ef658c39ee91a5abab28ca60
Juan Aleman <juan.j.aleman@gmail.com>
2020-05-05 21:20:50 -0500

    adding garys code to master and fixing routing

--- quake/src/App.js
+++ quake/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
-import Register from './components/Register';
-import Login from './components/Login'
-import Contact from './components/Contact';
-import Nav from './components/Nav';
-import Home from './components/Home';
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Contact from "./components/Contact";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path = '/'component = {Home}/>
-        <Route exact path = '/register' component ={Register}/>
-       <Route exact path = '/login' component ={Login}/>
-       <Route exact path = '/contact' component ={Contact}/>
-
-
-     </Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/contact" component={Contact} />
+      </Router>
     </div>
   );
 }
--- quake/src/components/Nav.js
+++ quake/src/components/Nav.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
+const Nav = () => {
+  return (
+    <div className="nav">
+      <NavLink className="navie" to="/">
+        Home
+      </NavLink>
 
-const Nav = ()=> {
+      <NavLink className="navie" to="/Login">
+        Login
+      </NavLink>
+      <NavLink className="navie" to="/register">
+        Register
+      </NavLink>
+      <NavLink className="navie" to="/contact">
+        Contact
+      </NavLink>
+    </div>
+  );
+};
 
-
-    return(
-
-        <div className = 'nav'>
-                    <NavLink className = 'navie'to = "/">Home</NavLink>
-
-        <NavLink className = 'navie'to = "/Login">Login</NavLink>
-        <NavLink className = 'navie' to = "/register">Register</NavLink>
-        <NavLink className = 'navie'to = "/contact">Contact</NavLink>
-
-        </div>
-    )
-}
-
-export default Nav
+export default Nav;

30ae7feaf26f6cfb2a0f4910711b9e78c2638f1e
RyanSWest <west56309@gmail.com>
2020-06-01 22:49:47 -0500

    added search by zipcode.

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,6 +4,19 @@ import axios from "axios";
 const Search = (props) => {
   const [place, setPlace] = useState({ city: "" });
 
+  const [zip, setZip] = useState({postcode:  ""});
+
+  const [zipMessage, setZipMessage] = useState({message: ''})
+
+
+  useEffect(()=> {
+    axios
+    .get(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/81503.json?limit=2&access_token=pk.eyJ1IjoicnN3ODg4IiwiYSI6ImNrYWRhZmZ5NTA1eGcycmxkdTRnNWFhbHgifQ.svdNU6YRgTECe5sPaLxeMg`
+    )
+    .then(res=> console.log("NEWREZ", res.data))
+  })
+
  
   const doSearch = (city) => {
     axios
@@ -11,6 +24,7 @@ const Search = (props) => {
         `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?limit=2&access_token=pk.eyJ1IjoicnN3ODg4IiwiYSI6ImNrYWRhZmZ5NTA1eGcycmxkdTRnNWFhbHgifQ.svdNU6YRgTECe5sPaLxeMg`
       )
       .then((res) => {
+        // console.log("THIS ZIPCODE",res.data.features[1].context[0].text)
          props.setUserCoords(res.data.features[0].center);
       });
   };
@@ -24,12 +38,29 @@ const Search = (props) => {
     });
   };
 
+   const handleZipInput = (e)=> {
+     setZip({
+     ...zip,
+     [e.target.name]: e.target.value})
+
+
+   }
    
 
   const onSubmit = (e) => {
     e.preventDefault();
     doSearch(place.city);
   };
+  const submitZip = e => {
+    e.preventDefault();
+    setZipMessage({message:''})
+    if(zip.postcode.length ==5){
+    doSearch(zip.postcode)}
+    else{
+      console.log( 'Please enter a valid Zipcode.', zip.postcode.length)
+      setZipMessage({message: "Please enter a valid Zipcode."})
+    }
+  }
   return (
     <>
       <form type="submit" onSubmit={onSubmit}>
@@ -40,8 +71,20 @@ const Search = (props) => {
           value={place.city}
           placeholder="Search by City"
         />
-
+        
         <button type="submit">Enter</button>
+      </form>
+
+      <form onSubmit ={submitZip}>
+      <input
+           type = 'text'
+          onChange = {handleZipInput}
+          name="postcode"
+          value={zip.postcode}
+          placeholder="Search by Zipcode"
+          />
+        <button type="submit">Enter</button>
+        <h3>{zipMessage.message}</h3>
       </form>
 
       

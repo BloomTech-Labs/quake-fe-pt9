3fe930d733d0de10f46e0ae597f9267751a13c82
garybot <argot.pryer@gmail.com>
2020-06-08 19:33:59 -0700

    Zoom based on searched place type.

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,45 +2,58 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const Search = (props) => {
-  const [place, setPlace] = useState({ city: "" });
+  const [place, setPlace] = useState("");
 
-
-  const doSearch = (city) => {
+  const doSearch = (place) => {
     axios
-      .get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?limit=2&access_token=pk.eyJ1IjoicnN3ODg4IiwiYSI6ImNrYWRhZmZ5NTA1eGcycmxkdTRnNWFhbHgifQ.svdNU6YRgTECe5sPaLxeMg`)
+      .get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?limit=2&access_token=pk.eyJ1IjoicnN3ODg4IiwiYSI6ImNrYWRhZmZ5NTA1eGcycmxkdTRnNWFhbHgifQ.svdNU6YRgTECe5sPaLxeMg`)
       .then((res) => {
-         props.setUserCoords(res.data.features[0].center);
+        console.log(res);
+        const features = res.data.features;
+        if (features.length) {
+          props.setUserData({
+            ...props.userData,
+            coords: res.data.features[0].center,
+            zoom: zoom(features[0].place_type[0])
+          });
+        } else {
+          alert("Enter a valid place name or postal code.")
+        }
       });
   };
 
+  const zoom = (placeType) => {
+    if (placeType === "place") {
+      return 10;
+    } else if (placeType === "region") {
+      return 6;
+    } else if (placeType === "country") {
+      return 6;
+    } else {
+      return 1;
+    }
+  }
+
   const handleInput = (e) => {
-    setPlace({
-      ...place,
-      [e.target.name]: e.target.value,
-    });
+    setPlace(e.target.value);
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    doSearch(place.city);
+    doSearch(place);
   };
 
   return (
-    <>
-      <form type="submit" onSubmit={onSubmit}>
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           onChange={handleInput}
-          name="city"
-          value={place.city}
-          placeholder="Search by City"
+          name="place"
+          value={place}
+          placeholder="City/Zip"
         />
-
         <button type="submit">Enter</button>
       </form>
-
-
-    </>
   );
 };
 
